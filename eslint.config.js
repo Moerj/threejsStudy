5cedfe0ae0bf51213d82d3fd4a9758ad01103f3e
Moer <199525807@qq.com>
2025-05-08 16:16:49 +0800

    物体点击

--- eslint.config.js
+++ eslint.config.js
@@ -19,6 +19,7 @@ export default [
                 requestAnimationFrame: 'readonly',
                 cancelAnimationFrame: 'readonly',
                 location: 'readonly',
+                console: 'readonly',
             }
         },
         rules: {
--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,10 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { onMounted, onBeforeUnmount } from 'vue'
 import { GUI } from "three/examples/jsm/libs/lil-gui.module.min.js"
 
-
-let animationFrameId
-
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 0, 3);
+camera.position.set(0, 0, 6);
 
 const renderer = new THREE.WebGLRenderer({
     antialias: true
@@ -22,7 +19,8 @@ document.querySelector("#app").appendChild(renderer.domElement);
 
 // 图片纹理
 const textureLoader = new THREE.TextureLoader();
-const texture = textureLoader.load('/shuye.jpg');
+const texture1 = textureLoader.load('/shuye.jpg');
+const texture2 = textureLoader.load('/ayaka.jpg');
 
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(ambientLight);
@@ -32,16 +30,26 @@ directionalLight.position.set(0, 0, 5);
 scene.add(directionalLight);
 
 // 几何体
-const geometry = new THREE.BoxGeometry()
+const geometry = new THREE.SphereGeometry(1, 32, 32)
 
 // 材质
-const material = new THREE.MeshStandardMaterial({
-    map: texture
+const material1 = new THREE.MeshStandardMaterial({
+    map: texture1
+});
+const material2 = new THREE.MeshStandardMaterial({
+    map: texture2
 });
 
-// 立方体
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// 3个球
+const spheres = [];
+const positions = [-3, 0, 3];  // x轴位置
+
+positions.forEach((x) => {
+    const sphere = new THREE.Mesh(geometry, material1.clone());
+    sphere.position.x = x;
+    spheres.push(sphere);
+    scene.add(sphere);
+});
 
 // 坐标辅助器
 const axesHelper = new THREE.AxesHelper(2);
@@ -51,15 +59,49 @@ scene.add(axesHelper);
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.enableDamping = true //旋转阻尼
 
+let animationFrameId
 function animate() {
     animationFrameId = requestAnimationFrame(animate);
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
-    renderer.render(scene, camera)
+    spheres.forEach(sphere => {
+        sphere.rotation.x += 0.01;
+        sphere.rotation.y += 0.01;
+    });
+    renderer.render(scene, camera);
 }
-
 animate()
 
+
+// 射线
+const raycaster = new THREE.Raycaster()
+const mouse = new THREE.Vector2()
+
+const cliclHandler = (e) => {
+    mouse.x = (e.clientX / window.innerWidth) * 2 - 1
+    mouse.y = -(e.clientY / window.innerHeight) * 2 + 1
+    raycaster.setFromCamera(mouse, camera)
+    const intersects = raycaster.intersectObjects(spheres)
+
+    if (intersects.length > 0) {
+        // 先还原所有材质
+        spheres.forEach(sphere => {
+            sphere.material.dispose();
+            sphere.material = material1.clone();
+        });
+
+        let obj = intersects[0].object;
+        if (obj.isMesh) {
+            obj.material.dispose();
+            obj.material = material2.clone();
+        }
+
+        // 刷新所有球体
+        spheres.forEach(sphere => {
+            sphere.needsUpdate = true;
+        });
+    }
+
+}
+window.addEventListener("click", cliclHandler)
 
 // 保存代码时,刷新three
 onBeforeUnmount(() => {
@@ -68,35 +110,19 @@ onBeforeUnmount(() => {
 
     // 清除几何体和材质
     geometry.dispose()
-    material.dispose()
-    texture.dispose()
+    material1.dispose()
+    texture1.dispose()
 
     // 清除渲染器
     renderer.dispose()
-
     // 移除渲染器的 DOM 元素
     renderer.domElement.remove()
+
+    controls.dispose()
+
+    window.removeEventListener("click", cliclHandler)
 })
 
-// GUI调试器
-let screenHandler ={
-    FullScreen: function (){
-        document.body.requestFullscreen()
-    },
-    ExitFullScreen: function(){
-        document.exitFullscreen()
-    }
-}
-
-const gui = new GUI()
-gui.add(screenHandler, "FullScreen").name("全屏")
-gui.add(screenHandler, "ExitFullScreen").name("退出全屏")
-
-// 控制立方体位置
-let folder = gui.addFolder("位置")
-folder.add(cube.position, "x").min(-5).max(10).name("x坐标")
-folder.add(cube.position, "y").min(-5).max(10).name("y坐标")
-folder.add(cube.position, "z").min(-5).max(10).name("z坐标")
 
 </script>
 
